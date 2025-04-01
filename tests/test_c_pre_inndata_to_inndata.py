ea309a95cbdd4a6e8b2896a0718025dcfeab1a8d
Arne Sørli <81353974+arneso-ssb@users.noreply.github.com>
2025-04-01 15:18:19 +0200

    Add testcase for autocorrect

--- tests/test_c_pre_inndata_to_inndata.py
+++ tests/test_c_pre_inndata_to_inndata.py
@@ -22,6 +22,25 @@ class TestProcessWeatherStationFile:
         facit_df = pd.read_parquet(facit_file)
         pd.testing.assert_frame_equal(result_df, facit_df)
 
+    def test_handles_validation_errors_with_autocorrection(
+        self, mocker: MockerFixture, tmp_path: Path, ws_autocorrect: pd.DataFrame
+    ) -> None:
+        mock_read_parquet = mocker.patch(
+            "notebooks.c_pre_inndata_to_inndata.read_parquet_file"
+        )
+        mock_read_parquet.return_value = ws_autocorrect
+
+        pre_inndata_filename = "weather_stations_pre_inndata_v1.parquet"
+        pre_inndata_file = Path(__file__).parent / "testdata" / pre_inndata_filename
+
+        process_weather_station_file(pre_inndata_file, tmp_path)
+        result_df = pd.read_parquet(tmp_path / pre_inndata_filename)
+
+        assert mock_read_parquet.call_count == 1
+        assert (
+            "SN499999010" not in result_df["id"].values
+        )  # Assert no rows with id 'SN499999010'
+
 
 class TestProcessObservationFile:
 
