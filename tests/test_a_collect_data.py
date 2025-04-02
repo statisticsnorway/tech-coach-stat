850b3a91d0fcd6192211c4cce82233fa21ecfd13
Arne Sørli <81353974+arneso-ssb@users.noreply.github.com>
2025-04-02 12:57:31 +0200

    Add tests for get_observations

--- tests/test_a_collect_data.py
+++ tests/test_a_collect_data.py
@@ -1,10 +1,13 @@
 from datetime import date
+from datetime import timedelta
 
 import pytest
 from google.auth.exceptions import DefaultCredentialsError
+from pytest_mock import MockerFixture
 
 from notebooks.a_collect_data import extract_latest_date_from_filename
 from notebooks.a_collect_data import frost_client_id
+from notebooks.a_collect_data import get_observations
 
 
 class TestExtractLatestDateFromFilename:
@@ -34,7 +37,7 @@ class TestExtractLatestDateFromFilename:
 class TestFrostClientId:
 
     # Successfully retrieves client_id from Google Secret Manager
-    def test_retrieves_client_id_from_gsm(self, mocker):
+    def test_retrieves_client_id_from_gsm(self, mocker: MockerFixture) -> None:
         mock_get_secret = mocker.patch(
             "notebooks.a_collect_data.get_secret_version", return_value="test-client-id"
         )
@@ -43,7 +46,7 @@ class TestFrostClientId:
         assert result == "test-client-id"
 
     # Handles DefaultCredentialsError when GSM credentials are not available
-    def test_gsm_secret_not_available(self, mocker):
+    def test_gsm_secret_not_available(self, mocker: MockerFixture) -> None:
         mock_get_secret = mocker.patch(
             "notebooks.a_collect_data.get_secret_version",
             side_effect=DefaultCredentialsError(),
@@ -55,3 +58,57 @@ class TestFrostClientId:
         mock_get_secret.assert_called_once()
         mock_getenv.assert_called_once_with("FROST_CLIENT_ID")
         assert result == "fallback-client-id"
+
+
+class TestGetObservations:
+
+    # Successfully retrieves observations when source_ids_ is provided and there are new observations
+    def test_retrieves_observations_when_new_data_available(
+        self, mocker: MockerFixture
+    ) -> None:
+        # Arrange
+        source_ids = ["SN18700", "SN18701"]
+        mock_today = date(year=2025, month=4, day=2)
+        mock_latest_date = mock_today - timedelta(days=5)
+
+        # Mock dependencies
+        mocker.patch(
+            "notebooks.a_collect_data.get_latest_observation_date",
+            return_value=mock_latest_date,
+        )
+        mocker.patch("notebooks.a_collect_data.date").today.return_value = mock_today
+        mock_fetch_data = mocker.patch("notebooks.a_collect_data.fetch_data")
+        mock_observations = [
+            {"referenceTime": f"{mock_latest_date + timedelta(days=1)}T00:00:00.000Z"},
+            {"referenceTime": f"{mock_today}T00:00:00.000Z"},
+        ]
+        mock_fetch_data.return_value = mock_observations
+
+        mocker.patch(
+            "notebooks.a_collect_data.extract_timespan",
+            return_value="2025-03-29_p2025-04-02",
+        )
+        mock_write_json = mocker.patch("notebooks.a_collect_data.write_json_file")
+
+        # Act
+        result = get_observations(source_ids)
+
+        # Assert
+        expected_parameters = {
+            "sources": "SN18700,SN18701",
+            "elements": (
+                "min(air_temperature P1D),"
+                "mean(air_temperature P1D),"
+                "max(air_temperature P1D),"
+                "sum(precipitation_amount P1D),"
+                "max(wind_speed P1D)"
+            ),
+            "referencetime": f"{(mock_latest_date + timedelta(days=1)).isoformat()}/{mock_today.isoformat()}",
+            "levels": "default",
+            "timeoffsets": "default",
+        }
+        mock_fetch_data.assert_called_once_with(
+            "https://frost.met.no/observations/v0.jsonld", expected_parameters
+        )
+        mock_write_json.assert_called_once()
+        assert result == mock_observations
