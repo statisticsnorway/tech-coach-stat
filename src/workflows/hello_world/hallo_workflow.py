b179f23b177d90a6148677fb59fab8e6f4066084
heidisu <hmr@ssb.no>
2026-06-03 14:24:56 +0200

    add hello world workflow example

---
+++ src/workflows/hello_world/hallo_workflow.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.decorators import task
+with DAG(
+    dag_id="hallo_workflow",
+    schedule=None,
+    start_date=datetime(2024, 1, 1),
+    catchup=False,
+) as dag:
+    @task(task_id="my_task")
+    def my_task():
+        print("Hello from Airflow workflow!")
+    task_1 = my_task()
---
+++ src/workflows/hello_world/upload.py
@@ -0,0 +1,13 @@
+from google.cloud import storage
+# Change these variables
+COMPOSER_BUCKET = "ssb-tip-tutorials-automation-composer"
+SOURCE_FILE_PATH= "src/workflows/hello_world/hallo_workflow.py"
+DESTINATION_PATH = "dags/hallo_workflow.py"
+def upload_file(bucket_name, source_file_path, destination_blob_name):
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+    blob.upload_from_filename(source_file_path)
+    print(f"File {source_file_path} uploaded to {destination_blob_name}.")
+if __name__ == "__main__":
+    upload_file(COMPOSER_BUCKET, SOURCE_FILE_PATH, DESTINATION_PATH)
